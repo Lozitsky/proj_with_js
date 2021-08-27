3a9afe6b9d2c945226191b6694bb21f8b495162e
Lozitsky <bondxxl@ukr.net>
2021-08-27 23:03:52 +0300

    Added Entire Meal Functionality. Шьздуьутеув Error Handling and Clear Button.

--- module1/whats-for-dinner/src/main.js
+++ module1/whats-for-dinner/src/main.js
@@ -1,10 +1,13 @@
 // Create variables targetting the relevant DOM elements here 👇
-const cookButton = document.querySelector("[type='submit']");
+const cookBtn = document.querySelector("[type='submit']");
+const clearBtn = document.querySelector("[type='reset']");
 const form = document.querySelector("form");
 const main = document.querySelector("main");
+const mealSection = main.lastElementChild;
 
 // Add your event listeners here 👇
-cookButton.addEventListener('click', makeMeal);
+cookBtn.addEventListener('click', makeMeal);
+clearBtn.addEventListener('click', clearMeal);
 
 // Create your event handlers and other functions here 👇
 
@@ -30,6 +33,8 @@ function getRandomMeal(meal_id) {
             return mains[getRandomIndex(mains)];
         case 'meal3':
             return desserts[getRandomIndex(desserts)];
+        case 'meal4':
+            return [sides[getRandomIndex(sides)], mains[getRandomIndex(mains)], desserts[getRandomIndex(desserts)]];
         default:
             return undefined;
     }
@@ -38,6 +43,45 @@ function getRandomMeal(meal_id) {
 function makeMeal(event) {
     event.preventDefault();
     let randomMeal = getRandomMeal(getSelectedId());
-    main.lastElementChild.innerHTML = `<em>You should make:</em><span>${randomMeal}!</span><button type="reset">CLEAR</button>`;
+    if (!randomMeal) {
+        return undefined;
+    }
+
+    mealSection.children[0].hidden = true;
+    for (let i = 1; i < mealSection.children.length; i++) {
+        mealSection.children[i].hidden = false;
+    }
+    // toggleVisibility();
+
+    if (typeof randomMeal === "string") {
+        clearP();
+        mealSection.children[2].textContent = `${randomMeal}!`;
+    } else {
+        clearSpan();
+        // mealSection.children[3].textContent = `${mains[7]} with a side of ${sides[0]} and ${desserts[1]} for desert!`;
+        mealSection.children[3].textContent = `${randomMeal[1]} with a side of ${randomMeal[0]} and ${randomMeal[2]} for desert!`;
+    }
     return randomMeal;
+}
+
+function toggleVisibility() {
+    let isImgHidden = mealSection.children[0].hidden;
+    mealSection.children[0].hidden = !isImgHidden;
+    for (let i = 1; i < mealSection.children.length; i++) {
+        mealSection.children[i].hidden = isImgHidden;
+    }
+}
+
+function clearMeal() {
+    toggleVisibility();
+    clearSpan();
+    clearP();
+}
+
+function clearSpan() {
+    mealSection.children[2].textContent = '';
+}
+
+function clearP() {
+    mealSection.children[3].textContent = '';
 }
