0bb1869db2f206754521f56c95c471862c8383fa
Lozitsky <bondxxl@ukr.net>
2021-09-21 21:24:04 +0300

    Finished Dragon class and passed the tests.

--- javascript-foundations/mythical-creatures/exercises/dragon.js
+++ javascript-foundations/mythical-creatures/exercises/dragon.js
@@ -1,1 +1,18 @@
+class Dragon {
+    constructor(name, rider) {
+        this.rider = rider;
+        this.name = name;
+        this.hungry = true;
+        this.count = 0;
+    }
 
+    greet() {
+        return `Hi, ${this.rider}!`;
+    }
+
+    eat() {
+        this.hungry = ++this.count < 3;
+    }
+}
+
+module.exports = Dragon;
