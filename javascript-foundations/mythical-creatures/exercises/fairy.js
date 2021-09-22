20816d12c5eb328a9b28c4fb62e1028e9db0517b
Lozitsky <bondxxl@ukr.net>
2021-09-22 09:07:56 +0300

    Finished Fairy class and passed the tests.

---
+++ javascript-foundations/mythical-creatures/exercises/fairy.js
@@ -0,0 +1,38 @@
+class Fairy {
+    constructor(name) {
+        this.name = name;
+        this.dust = 10;
+        this.clothes = {dresses: ['Iris']};
+        this.disposition = "Good natured";
+        this.humanWards = [];
+    }
+
+    receiveBelief() {
+        this.dust++;
+    }
+
+    believe() {
+        this.dust += 10;
+    }
+
+    makeDresses(strings) {
+        this.clothes = {dresses: [...this.clothes.dresses, ...strings]};
+    }
+
+    becomeProvoked() {
+        this.disposition = "Vengeful";
+    }
+
+    replaceInfant(infant) {
+        if (this.disposition === "Vengeful") {
+            infant.disposition = "Malicious";
+            this.humanWards.push(infant);
+            if (this.humanWards.length > 2) {
+                this.disposition = "Good natured";
+            }
+        }
+        return infant;
+    }
+}
+
+module.exports = Fairy;
