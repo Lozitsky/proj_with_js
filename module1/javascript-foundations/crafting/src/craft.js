6d250669d2da7ad8f86cfc37be313c285b3e0140
Lozitsky <bondxxl@ukr.net>
2021-09-23 03:26:00 +0300

    Did all the tests. Material and Сraft classes were completed.

--- module1/javascript-foundations/crafting/src/craft.js
+++ module1/javascript-foundations/crafting/src/craft.js
@@ -1,5 +1,21 @@
 class Craft {
+    constructor(obj) {
+        this.name = obj.type;
+        this.materials = [];
+        this.completed = false;
+        if (obj.materials) {
+            this.materials = obj.materials;
+        }
+    }
 
+    completeCraft() {
+        this.completed = true;
+        return "All done! It looks great!";
+    }
+
+    calculateProjectTotal() {
+        return this.materials.reduce((pv, nv) => pv + nv.calculateMaterialCost(), 0);
+    }
 }
 
 module.exports = Craft;
