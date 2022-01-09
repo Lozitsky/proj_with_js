b8482852abb921ec984f348372c9524525a3eb9c
Lozitsky <bondxxl@ukr.net>
2022-01-09 13:04:53 +0200

    Created RecipeRepository class and tests for it.

--- module2/whats-cookin/src/classes/RecipeRepository.js
+++ module2/whats-cookin/src/classes/RecipeRepository.js
@@ -1,8 +1,28 @@
-class RecipeRepository {
-  constructor() {
+// import ingredients from '../data/ingredients';
+// import recipe from "./Recipe";
+const { ingredientsData } = require('../data/ingredients');
 
-    // One class to get you started!
+
+
+class RecipeRepository {
+  constructor(recipes) {
+    this.recipes = recipes;
   }
+
+  getRecipesByTags(...tags) {
+    return this.recipes.filter(recipe => tags.some(el => 
+      el === '' ? !recipe.tags.length : recipe.tags.includes(el)));
+  }
+
+  getRecipeByName(name) {
+    return this.recipes.find(recipe => recipe.name === name);
+  }
+
+  getRecipesByIngredients(...ingreds) {
+    return this.recipes.filter(recipe => ingreds.every(name => recipe.ingredients.some(ingred => 
+      ingred.id === ingredientsData.find(ing => ing.name === name).id)));
+  }
+
 }
 
 export default RecipeRepository;
