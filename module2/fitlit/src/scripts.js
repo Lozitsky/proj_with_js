2351e635f14ac9d2cedd9dfa6f6a9d7ecc4016af
Lozitsky <bondxxl@ukr.net>
2022-04-25 17:27:49 +0300

    Initialized the fitlit project

---
+++ module2/fitlit/src/scripts.js
@@ -0,0 +1,17 @@
+// This is the JavaScript entry file - your code begins here
+// Do not delete or rename this file ********
+
+console.log(userData,"<>>>>userData")
+// An example of how you tell webpack to use a CSS file
+import './css/styles.css';
+
+// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import './images/turing-logo.png'
+
+console.log('This is the JavaScript entry file - your code begins here.');
+
+// An example of how you tell webpack to use a JS file
+
+import userData from './data/users';
+
+import UserRepository from './UserRepository';
