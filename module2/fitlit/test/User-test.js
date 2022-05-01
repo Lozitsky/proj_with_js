874609afd23e4c7b20466d90cd9ae4e8c07b16f6
Lozitsky <bondxxl@ukr.net>
2022-05-01 15:47:28 +0300

    Added tests for other functions. Cleaned code. Added aria attributes.

--- module2/fitlit/test/User-test.js
+++ module2/fitlit/test/User-test.js
@@ -22,36 +22,68 @@ describe('User', () => {
   it('should be a function', function () {
     expect(User).to.be.a('function');
   });
-  it('should be an instance of User', function () {
-    const user = new User();
-    expect(user).to.be.an.instanceof(User);
-  });
-  it('should have a userData property', function () {
-    const user = new User();
-    expect(user).to.have.property('userData');
-  });
-  it('should hold a userData object', function () {
-    const user = new User(user1);
-    expect(user).to.have.deep.property('userData', {
-      "id": 1,
-      "name": "Luisa Hane",
-      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-      "email": "Diana.Hayes1@hotmail.com",
-      "strideLength": 4.3,
-      "dailyStepGoal": 10000,
-      "friends": [
-        16,
-        4,
-        8
-      ]
+
+  describe('User()', () => {
+    let user;
+    beforeEach(() => {
+      user = new User();
+    });
+    it('should be an instance of User', function () {
+      expect(user).to.be.an.instanceof(User);
+    });
+    it('should have a userData property', function () {
+      // const user = new User();
+      expect(user).to.have.property('userData');
+    });
+    it('should have method `getName`', function () {
+      const user = new User();
+      expect(user).itself.to.respondsTo('getName');
     });
   });
-  it('should have method `getName`', function () {
-    const user = new User();
-    expect(user).itself.to.respondsTo('getName');
-  });
-  it('should return a user’s first name only', function () {
-    const user = new User(user1);
-    expect(user.getName()).to.be.a('string').eq('Luisa');
-  });
+
+  describe('User(user1)', () => {
+    let user;
+    beforeEach(() => {
+      user = new User(user1);
+    });
+    it('should hold a userData object', function () {
+      // const user = new User(user1);
+      expect(user).to.have.deep.property('userData', {
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+          16,
+          4,
+          8
+        ]
+      });
+    });
+    it('should return a user’s first name only', function () {
+      // const user = new User(user1);
+      expect(user.getName()).to.be.a('string').eq('Luisa');
+    });
+    it('should return full name', function () {
+      // const user = new User(user1);
+      expect(user.getFullname()).to.be.a('string').eq('Luisa Hane');
+    });
+    it('should return an address', function () {
+      expect(user.getAddress()).to.be.a('string').eq('15195 Nakia Tunnel, Erdmanport VA 19901-1697');
+    });
+    it('should return an email', function () {
+      expect(user.getEmail()).to.be.a('string').eq('Diana.Hayes1@hotmail.com');
+    });
+    it('should return a stride length', function () {
+      expect(user.getStrideLength()).to.be.a('number').eq(4.3);
+    });
+    it('should return a daily step goal', function () {
+      expect(user.getDailyStepGoal()).to.be.a('number').eq(10000);
+    });
+    it('should return friends', function () {
+      expect(user.getFriends()).to.be.an('array').deep.eq([16, 4, 8]);
+    });
+  })
 });
--- module2/fitlit/test/UserRepository-test.js
+++ module2/fitlit/test/UserRepository-test.js
@@ -6,50 +6,54 @@ describe('User Repository', () => {
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
-  it('should be an instance of UserRepository', function () {
-    const repo = new UserRepository();
-    expect(repo).to.be.an.instanceof(UserRepository);
-  });
-  it('should have a data property', function () {
-    const repo = new UserRepository();
-    expect(repo).to.have.property('data');
-  })
-  it('should hold all of the User objects', function () {
-    let repo = new UserRepository(users);
-    expect(repo).to.have.deep.property('data', users);
-  });
-  it('should have method `getUserData`', function () {
-    const repo = new UserRepository();
-    expect(repo).itself.to.respondsTo('getUserData');
-  });
-  it('should have method `getAverageStepGoal`', function () {
-    const repo = new UserRepository();
-    expect(repo).itself.to.respondsTo('getAverageStepGoal');
-  });
-  it('should return a user data', () => {
-    const repo = new UserRepository(users);
-    expect(repo.getUserData(13)).to.deep.equal({
-      "id": 13,
-      "name": "Tom Schmeler",
-      "address": "1524 Clemmie River, Newtonbury RI 02849-3159",
-      "email": "Leopoldo.Sauer@gmail.com",
-      "strideLength": 3.2,
-      "dailyStepGoal": 4000,
-      "friends": [
-        33,
-        14,
-        3,
-        43,
-        35
-      ]
+  describe('UserRepository()', () => {
+    let repo;
+    beforeEach(() => {
+      repo = new UserRepository();
+    });
+    it('should be an instance of UserRepository', function () {
+      expect(repo).to.be.an.instanceof(UserRepository);
+    });
+    it('should have a data property', function () {
+      expect(repo).to.have.property('data');
+    });
+    it('should have method `getUserData`', function () {
+      expect(repo).itself.to.respondsTo('getUserData');
+    });
+    it('should have method `getAverageStepGoal`', function () {
+      expect(repo).itself.to.respondsTo('getAverageStepGoal');
     });
   });
-  it('should return quantity', function () {
-    const repo = new UserRepository(users);
-    expect(repo.getQuantity()).to.equal(50);
-  });
-  it('should return average step goal', function () {
-    const repo = new UserRepository(users);
-    expect(repo.getAverageStepGoal()).to.equal(6700);
-  });
+  describe('UserRepository(users)', () => {
+    let repo;
+    beforeEach(() => {
+      repo = new UserRepository(users);
+    });
+    it('should hold all of the User objects', function () {
+      expect(repo).to.have.deep.property('data', users);
+    });
+    it('should return a user data', () => {
+      expect(repo.getUserData(13)).to.deep.equal({
+        "id": 13,
+        "name": "Tom Schmeler",
+        "address": "1524 Clemmie River, Newtonbury RI 02849-3159",
+        "email": "Leopoldo.Sauer@gmail.com",
+        "strideLength": 3.2,
+        "dailyStepGoal": 4000,
+        "friends": [
+          33,
+          14,
+          3,
+          43,
+          35
+        ]
+      });
+    });
+    it('should return quantity', function () {
+      expect(repo.getQuantity()).to.equal(50);
+    });
+    it('should return average step goal', function () {
+      expect(repo.getAverageStepGoal()).to.equal(6700);
+    });
+  })
 });
