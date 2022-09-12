c3cdbe348c0eae8c60a56fbc290e8389ecd3f5bd
Lozitsky <bondxxl@ukr.net>
2022-09-12 23:32:50 +0300

    Created 0 iteration

--- test/Box-test.js
+++ test/Box-test.js
@@ -1,3 +1,9 @@
-// const chai = require('chai');
-// const expect = chai.expect;
+ const chai = require('chai');
+ const expect = chai.expect;
 // const Box = require('../src/Box');
+
+describe('first test', () =>
+	it('should be pass', () =>
+		expect(true).to.equal(true)
+	)
+)
