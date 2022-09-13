4e35bddf1edeed7f282365bb7560e5ce41870087
Lozitsky <bondxxl@ukr.net>
2022-09-13 16:23:59 +0300

    Created iteration 1

--- src/Box.js
+++ src/Box.js
@@ -1,4 +1,13 @@
+class Box {
+	constructor(height = 100, width = 100) {
+		this.height = height;
+		this.width = width;
+	}
+
+	calculateArea() {
+		return this.height * this.width;
+	}
+}
 
 
-
-// module.exports = Box;
+ module.exports = Box;
--- test/Box-test.js
+++ test/Box-test.js
@@ -1,9 +1,28 @@
  const chai = require('chai');
  const expect = chai.expect;
-// const Box = require('../src/Box');
+ const Box = require('../src/Box');
 
-describe('first test', () =>
-	it('should be pass', () =>
-		expect(true).to.equal(true)
-	)
-)
+describe('Box', () => {
+	var box = new Box();
+	it('should have constructor named Box', () => {
+		expect(box).to.be.instanceOf(Box);
+	});
+
+	it('constructor should has a default height and width of 100', () => {
+		expect(box).to.have.property('width', 100);
+		expect(box).to.have.property('height', 100);
+	});
+	it('should be able to pass in specific height and widths', function () {
+		var box = new Box(50, 60);
+		expect(box).to.property('width').to.equal(60);
+		expect(box).to.property('height').to.equal(50);
+	});
+	it('Box object should has a method .calculateArea()', function () {
+		expect(box).to.have.property('calculateArea');
+	});
+
+	it('.calculateArea() should be able to calculate the area', function () {
+		var box2 = new Box(50, 60);
+		expect(box2.calculateArea()).to.equal(3000);
+	});
+});
