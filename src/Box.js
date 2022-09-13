88fa38daed057beb69c03824e3567a61d3bfe8d7
Lozitsky <bondxxl@ukr.net>
2022-09-13 18:00:58 +0300

    Created iteration 2

--- src/Box.js
+++ src/Box.js
@@ -1,13 +1,21 @@
 class Box {
-	constructor(height = 100, width = 100) {
-		this.height = height;
-		this.width = width;
-	}
+    constructor(height = 100, width = 100) {
+        this.height = height;
+        this.width = width;
+    }
 
-	calculateArea() {
-		return this.height * this.width;
-	}
+    calculateArea() {
+        return this.height * this.width;
+    }
+
+    increaseWidth(value) {
+        this.width += value;
+    }
+
+    increaseHeight(value) {
+        this.height += value;
+    }
 }
 
 
- module.exports = Box;
+module.exports = Box;
