d4af37f8288c3c3c532d0191237b64d01c6719f4
Lozitsky <bondxxl@ukr.net>
2022-09-14 23:06:33 +0300

    Added linting, code refactoring

--- src/Rocket.js
+++ src/Rocket.js
@@ -1,24 +1,24 @@
 class Rocket {
-    constructor() {
-        this.name = '';
-        this.fuel = 'empty';
-        this.astronauts = [];
-        this.enginesIgnited = false;
-        this.cargoDoors = 'closed';
-        this.cargo = [];
-    }
+  constructor() {
+    this.name = '';
+    this.fuel = 'empty';
+    this.astronauts = [];
+    this.enginesIgnited = false;
+    this.cargoDoors = 'closed';
+    this.cargo = [];
+  }
 
-    loadAstronaut(name) {
-        this.astronauts.push(name);
-    }
+  loadAstronaut(name) {
+    this.astronauts.push(name);
+  }
 
-    lightThisCandle() {
-        this.enginesIgnited = true;
-    }
+  lightThisCandle() {
+    this.enginesIgnited = true;
+  }
 
-    fillTanks() {
-        this.fuel = 'full';
-    }
+  fillTanks() {
+    this.fuel = 'full';
+  }
 }
 
 export default Rocket;
